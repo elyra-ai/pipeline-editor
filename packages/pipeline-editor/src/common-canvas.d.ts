6c5546e955c93851a4878920dd516197c02a3da0
Nick Bourdakos <nicholas.bourdakos@ibm.com>
2021-02-25 15:04:57 -0500

    Add an option to use native keyboard actions

--- packages/pipeline-editor/src/common-canvas.d.ts
+++ packages/pipeline-editor/src/common-canvas.d.ts
@@ -120,6 +120,17 @@ declare module "@elyra/canvas" {
         createSupernode?: boolean;
       };
     };
+    keyboardConfig?: {
+      actions?: {
+        delete?: boolean;
+        undo?: boolean;
+        redo?: boolean;
+        selectAll?: boolean;
+        cutToClipboard?: boolean;
+        copyToClipboard?: boolean;
+        pasteFromClipboard?: boolean;
+      };
+    };
   }
 
   function CommonCanvas(props: CommonCanvasProps);
