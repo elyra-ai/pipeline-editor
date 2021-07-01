02360ae36b639a79f73545680edd988e56e577ab
Martha Cryan <martha.cryan@ibm.com>
2021-07-01 16:24:22 -0500

    Restrict type

--- packages/pipeline-editor/src/types.ts
+++ packages/pipeline-editor/src/types.ts
@@ -27,7 +27,7 @@ export interface CustomNodeSpecification {
   image?: string;
   properties?: PropertyDefinitions;
   id?: string;
-  type?: "super_node" | "execution_node" | "binding" | "model_node";
+  type?: "execution_node";
   inputs?: any[];
   outputs?: any[];
   parameters?: any;
